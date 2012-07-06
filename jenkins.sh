dfeda884c0a482659d056addd9ed526da7bc4064
Manoj Dayaram <i.am.noj@gmail.com>
2012-07-06 15:15:29 -0700

    Adding failure check on MOOV_HOME env vars.

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 [ -e $HOME/gobuilds/tools/jenkins-env.sh ] && source $HOME/gobuilds/tools/jenkins-env.sh
-[ -z $CLIBS_HOME ] && export CLIBS_HOME=$HOME/moovweb/clibs
+[ -z $CLIBS_HOME ] && [ ! -z $MOOV_HOME ] && export CLIBS_HOME=$MOOV_HOME/clibs
+if [ -z $CLIBS_HOME ]; then
+	echo "Please set CLIBS_HOME or MOOV_HOME before running this script."
+	exit 1
+fi
 [ ! -d $CLIBS_HOME ] && mkdir -p $CLIBS_HOME
 
 #make clean
